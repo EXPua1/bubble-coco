735b14a17d26fd40d108b4f2983c2c83c33da404
EXPua1 <161503064+EXPua1@users.noreply.github.com>
2024-10-06 01:15:37 +0000

    Deploying to gh-pages from @ EXPua1/bubble-coco@797d92585d6c21cf59411301c5f6ee9a81c79504 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/styles-5d481978.js";if(document.body.classList.contains("privacy-page")){const n=document.querySelector(".pr_btn");n?n.addEventListener("click",function(){window.location.href="./index.html"}):console.error("Элемент с классом '.pr_btn' не найден!")}
+//# sourceMappingURL=commonHelpers2.js.map
