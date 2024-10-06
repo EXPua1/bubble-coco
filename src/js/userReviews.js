4712687590122e552c5d0eb606debbaa759ac405
EXPua1 <161503064+EXPua1@users.noreply.github.com>
2024-10-06 12:49:11 +0300

    git

--- src/js/userReviews.js
+++ src/js/userReviews.js
@@ -1,47 +1,47 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const reviewWrapper = document.querySelector('.reviews_wrapper');
-  const reviewList = document.querySelector('.reviews_list');
-  const leftArrow = document.querySelector('.left_arrow');
-  const rightArrow = document.querySelector('.right_arrow');
+// document.addEventListener('DOMContentLoaded', function () {
+//   const reviewWrapper = document.querySelector('.reviews_wrapper');
+//   const reviewList = document.querySelector('.reviews_list');
+//   const leftArrow = document.querySelector('.left_arrow');
+//   const rightArrow = document.querySelector('.right_arrow');
 
-  let currentIndex = 0;
-  const totalCards = document.querySelectorAll('.review_card').length;
-  const maxIndex = totalCards - 3;
+//   let currentIndex = 0;
+//   const totalCards = document.querySelectorAll('.review_card').length;
+//   const maxIndex = totalCards - 3;
 
-  function updateArrows() {
-    leftArrow
-      .querySelector('use')
-      .setAttribute(
-        'href',
-        currentIndex === 0
-          ? '/img/sprite.svg#icon_left_Off'
-          : '/img/sprite.svg#icon_left_On'
-      );
-    rightArrow
-      .querySelector('use')
-      .setAttribute(
-        'href',
-        currentIndex === maxIndex
-          ? '/img/sprite.svg#icon_right_Off'
-          : '/img/sprite.svg#icon_right_On'
-      );
-  }
+//   function updateArrows() {
+//     leftArrow
+//       .querySelector('use')
+//       .setAttribute(
+//         'href',
+//         currentIndex === 0
+//           ? './img/sprite.svg#icon_left_Off'
+//           : '/img/sprite.svg#icon_left_On'
+//       );
+//     rightArrow
+//       .querySelector('use')
+//       .setAttribute(
+//         'href',
+//         currentIndex === maxIndex
+//           ? './img/sprite.svg#icon_right_Off'
+//           : './img/sprite.svg#icon_right_On'
+//       );
+//   }
 
-  rightArrow.addEventListener('click', function () {
-    if (currentIndex < maxIndex) {
-      currentIndex++;
-      reviewList.style.transform = `translateX(-${currentIndex * 368}px)`;
-      updateArrows();
-    }
-  });
+//   rightArrow.addEventListener('click', function () {
+//     if (currentIndex < maxIndex) {
+//       currentIndex++;
+//       reviewList.style.transform = `translateX(-${currentIndex * 368}px)`;
+//       updateArrows();
+//     }
+//   });
 
-  leftArrow.addEventListener('click', function () {
-    if (currentIndex > 0) {
-      currentIndex--;
-      reviewList.style.transform = `translateX(-${currentIndex * 368}px)`;
-      updateArrows();
-    }
-  });
+//   leftArrow.addEventListener('click', function () {
+//     if (currentIndex > 0) {
+//       currentIndex--;
+//       reviewList.style.transform = `translateX(-${currentIndex * 368}px)`;
+//       updateArrows();
+//     }
+//   });
 
-  updateArrows();
-});
+//   updateArrows();
+// });
