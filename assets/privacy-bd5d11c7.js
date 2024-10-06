38b254d66f2892300db401fbb99a19c0a4241513
AleksMarkov <137721438+AleksMarkov@users.noreply.github.com>
2024-10-06 04:56:27 +0000

    Deploying to gh-pages from @ EXPua1/bubble-coco@424362316ebc4321c54f6ac8d4e88fd2cb2cc17a 🚀

---
+++ assets/privacy-bd5d11c7.js
@@ -0,0 +1,2 @@
+if(document.body.classList.contains("privacy-page")){const n=document.querySelector(".pr_btn");n?n.addEventListener("click",function(){window.location.href="./index.html"}):console.error("Элемент с классом '.pr_btn' не найден!")}
+//# sourceMappingURL=privacy-bd5d11c7.js.map
